a319e2a83f0d11ab91870084edfdabb9597caf9d
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-04-14 01:15:45 +0200

    utils/WebServerTester - part 3

--- utils/WebServerTester/src/main/java/pl/mfalkowski/NodeMCUtests/NodeMcuTestsApplication.java
+++ utils/WebServerTester/src/main/java/pl/mfalkowski/NodeMCUtests/NodeMcuTestsApplication.java
@@ -1,11 +1,12 @@
 package pl.mfalkowski.NodeMCUtests;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,11 +14,11 @@ import reactor.core.publisher.Mono;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 
-import java.net.ResponseCache;
 import java.net.URI;
-import java.util.Map;
 import java.util.Objects;
 
+import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NON_PRIVATE;
+import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -26,6 +27,22 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class NodeMcuTestsApplication {
 
 	private Logger logger = Loggers.getLogger(NodeMcuTestsApplication.class);
+	private State state = new State();
+
+    static class State {
+	    String ssid;
+	    String password;
+	    String status;
+    }
+
+    static class Status {
+        Wifi wifi;
+
+        static class Wifi {
+            String ssid;
+            String status;
+        }
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(NodeMcuTestsApplication.class, args);
@@ -33,7 +50,14 @@ public class NodeMcuTestsApplication {
 
 	private HandlerFunction<ServerResponse> statusHandler =
         request ->
-            ServerResponse.ok().body(BodyInserters.fromObject("OK"));
+            ServerResponse.ok().body(
+                Mono.just(new Status())
+                    .doOnNext(s -> s.wifi = new Status.Wifi())
+                    .doOnNext(s -> s.wifi.status = state.status)
+                    .doOnNext(s -> s.wifi.ssid = state.ssid),
+                Status.class
+            );
+
 
 	private HandlerFunction<ServerResponse> connectHandler =
 		request ->
@@ -42,10 +66,12 @@ public class NodeMcuTestsApplication {
                     Mono.just(body.getFirst("ssid"))
                         .filter(Objects::nonNull)
                         .doOnNext(s -> logger.info("SSID: {}", s))
+                        .doOnNext(s -> state.ssid = s)
                         .switchIfEmpty(Mono.fromRunnable(() -> logger.info("No ssid in request!")))
                         .then(Mono.just(body.getFirst("password")))
                         .filter(Objects::nonNull)
                         .doOnNext(p -> logger.info("password: {}", p))
+                        .doOnNext(p -> state.password = p)
                         .switchIfEmpty(Mono.fromRunnable(() -> logger.info("No password in request!")))
                 )
                 .then(ServerResponse.seeOther(URI.create("/connecting")).build());
@@ -64,5 +90,9 @@ public class NodeMcuTestsApplication {
 			.and(route(POST("/connect"), connectHandler));
     }
 
-
+    @Bean
+	ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		return builder.build()
+			.setVisibility(FIELD, NON_PRIVATE);
+	}
 }
