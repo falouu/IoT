93c2ebb3117ae5422a1d7b0f35397962086c9c96
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-04-15 05:24:28 +0200

    utils/WebServerTester - part 6 - get templates

--- utils/WebServerTester/src/main/java/pl/mfalkowski/NodeMCUtests/NodeMcuTestsApplication.java
+++ utils/WebServerTester/src/main/java/pl/mfalkowski/NodeMCUtests/NodeMcuTestsApplication.java
@@ -4,24 +4,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mustache.MustacheProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerRequestExtensionsKt;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 import reactor.util.function.Tuple2;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NON_PRIVATE;
 import static com.fasterxml.jackson.annotation.PropertyAccessor.FIELD;
+import static org.springframework.web.reactive.function.BodyInserters.fromObject;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -50,6 +66,8 @@ public class NodeMcuTestsApplication {
 
 	    List<WifiStatusData> wifiStatuses;
 	    String wifiStatus;
+
+	    List<String> templateNames;
     }
 
     static class State {
@@ -67,7 +85,15 @@ public class NodeMcuTestsApplication {
         }
     }
 
-	public static void main(String[] args) {
+    private final MustacheProperties mustacheProperties;
+
+    @Autowired
+    public NodeMcuTestsApplication(MustacheProperties mustacheProperties) {
+        this.mustacheProperties = mustacheProperties;
+    }
+
+
+    public static void main(String[] args) {
 		SpringApplication.run(NodeMcuTestsApplication.class, args);
 	}
 
@@ -115,6 +141,11 @@ public class NodeMcuTestsApplication {
                 .doOnNext(t -> t.getT1().wifiStatuses = t.getT2())
                 .map(Tuple2::getT1)
                 .doOnNext(data -> data.wifiStatus = state.status)
+                .flatMap(data -> getTemplateNames()
+                    .collectList()
+                    .doOnNext(templateNames -> data.templateNames = templateNames)
+                    .map(t -> data)
+                )
                 .map(data -> Collections.singletonMap("data", data))
                 .flatMap(model -> ServerResponse.ok().render("admin/admin", model));
 
@@ -123,6 +154,25 @@ public class NodeMcuTestsApplication {
             request.formData()
                 .doOnNext(form -> state.status = form.getFirst("wifiStatus"))
                 .then(ServerResponse.seeOther(URI.create("/admin")).build());
+
+	private HandlerFunction<ServerResponse> adminGetTemplateHandler =
+        request ->
+            Flux.fromStream(request.queryParam("name").stream())
+                .singleOrEmpty()
+                .materialize()
+                .flatMap(signal ->
+                    signal.getType() == SignalType.ON_COMPLETE
+                        ? ServerResponse.badRequest().body(fromObject("'name' query param is missing!"))
+                        : getTemplate(signal.get())
+                            .flatMap(content -> ServerResponse.ok()
+                                .contentType(MediaType.TEXT_PLAIN)
+                                .body(fromObject(content))
+                            )
+                            .onErrorResume(error -> ServerResponse
+                                .status(HttpStatus.NOT_FOUND)
+                                .body(fromObject("Template '" + signal.get() + "' not found!"))
+                            )
+                );
 
     @Bean
     RouterFunction<ServerResponse> routes()  {
@@ -133,7 +183,8 @@ public class NodeMcuTestsApplication {
             .and(route(GET("/connecting"), request -> ServerResponse.ok().render("connecting")))
 			.and(route(POST("/connect"), connectHandler))
             .and(route(GET("/admin"), adminGetHandler))
-            .and(route(POST("/admin"), adminPostHandler));
+            .and(route(POST("/admin"), adminPostHandler))
+            .and(route(GET("/admin/template"), adminGetTemplateHandler));
     }
 
     @Bean
@@ -141,4 +192,38 @@ public class NodeMcuTestsApplication {
 		return builder.build()
 			.setVisibility(FIELD, NON_PRIVATE);
 	}
+
+	private Flux<String> getTemplateNames() {
+        String suffix = mustacheProperties.getSuffix();
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        List<Resource> resources;
+        try {
+            resources = Arrays.asList(resolver.getResources("classpath:/templates/*" + suffix));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Flux.fromIterable(resources)
+            .map(Resource::getFilename)
+            .map(filename -> filename.substring(0, filename.lastIndexOf('.')));
+    }
+
+    private Mono<String> getTemplate(String template) {
+
+        return Mono
+            .fromCallable(() ->
+                this.getClass().getResource("/templates/" + template + mustacheProperties.getSuffix())
+            )
+            .map(url -> {
+                try {
+                    return Files.lines(Paths.get(url.toURI()));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            })
+            .single()
+            .flatMapMany(Flux::fromStream)
+            .collect(Collectors.joining("\n"));
+    }
 }
--- utils/WebServerTester/src/test/java/pl/mfalkowski/NodeMCUtests/NodeMcuTestsApplicationTests.java
+++ utils/WebServerTester/src/test/java/pl/mfalkowski/NodeMCUtests/NodeMcuTestsApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +13,19 @@ public class NodeMcuTestsApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+
+	@Test
+	public void expandTest() {
+
+		Mono.just("A")
+			.expand(s ->
+				Mono.just(s)
+					.filter(ss -> ss.length() < 5)
+					.flatMapMany(ss -> Flux.just(ss + "!", ss + "?"))
+			)
+			.subscribe(System.out::println);
 	}
 
 }
