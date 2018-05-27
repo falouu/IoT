3bc4bd25a2dabf380aba908d458229e12453b6e3
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-05-27 19:14:25 +0200

    Update bashduino TODO list; clion command (in progress)

--- NodeMcuV3/bashduino/TODO.md
+++ NodeMcuV3/bashduino/TODO.md
@@ -1,5 +1,10 @@
 # TODO
 
+* autocompletion for command parameters values (not only parameter names)
+
+# DONE
+
+* Autocompletion for parameter names
 * `common.sh`: `import()` - move the function to a separate and exclusive file
   and source it, because now `eval` in that function mess with line numbering
   in `common.sh` file (it is important for `die()` function, when we show the
@@ -8,9 +13,3 @@
   * Hint: every function with `eval` should be in separate file. Maybe make
     one file with only `eval` function in it? And it will `eval` any argument
     it receive. To consider.
-
-* autocompletion for command parameters values (not only parameter names)
-
-# DONE
-
-* Autocompletion for parameter names
--- NodeMcuV3/bashduino/src/main/commands/clion.sh
+++ NodeMcuV3/bashduino/src/main/commands/clion.sh
@@ -5,11 +5,34 @@
 # Output variables:
 #   PARAMS | map | define params for command
 setup() {
-  return
+  required_variables "BASHDUINO_SRC_ROOT_DIR" "ROOT_DIR"
 }
 
 # Input variables
 #   ARGS | map | arguments values
 run() {
+    import "bashduino/snapshots/get_hardware_dir" as "get_hardware_dir"
 
+
+    get_hardware_dir
+    local hardware_dir="${RETURN_VALUE}"
+
+    local template_name="CMakeLists.template.txt"
+
+    local default_template_abs="${BASHDUINO_SRC_ROOT_DIR}/resources/${template_name}"
+    local custom_template_abs="${ROOT_DIR}/${template_name}"
+
+    if [[ ! -f "${custom_template_abs}" ]]; then
+        log "${template_name} file not found in root directory, creating one with default content..."
+        cp "${default_template_abs}" "${custom_template_abs}"
+        success || {
+            die "Creating ${template_name} file FAILED" "GENERAL/CANNOT_CREATE_FILE"
+        }
+    fi
+
+    local include_directories_block=""
+
+    
+
+    "include_directories("
 }
