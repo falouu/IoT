1f33afee144aa1eee74f98963c41af256b8356c0
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-06-06 21:52:02 +0200

    CaptivePortal extracting to library - operation step by step 6

--- NodeMcuV3/src/libraries/CaptivePortal/src/CaptivePortal.h
+++ NodeMcuV3/src/libraries/CaptivePortal/src/CaptivePortal.h
@@ -2,6 +2,7 @@
 #define NODEMCUV3_CAPTIVEPORTAL_H
 
 #include <Arduino.h>
+#include <ESP8266WebServer.h>
 
 class CaptivePortal {
 public:
@@ -34,6 +35,20 @@ private:
   void logRequest();
   bool preHandle();
   void updateState();
+
+  //// CONSTANTS /////////////////
+  // DNS server
+  const byte DNS_PORT = 53;
+  /* Soft AP network parameters */
+  const IPAddress apIP { IPAddress(192, 168, 1, 1) };
+  const IPAddress netMsk { IPAddress(255, 255, 255, 0) };
+  const String myHostname = "esp8266";
+
+  /* Set these to your desired softAP credentials. They are not configurable at runtime */
+  const char *softAP_ssid = "ESP_NODE_MCU";
+  const char *softAP_password = "cm52gn3k";
+
+  //// END OF CONSTANTS /////////////////
 };
 
 #endif //NODEMCUV3_CAPTIVEPORTAL_H
