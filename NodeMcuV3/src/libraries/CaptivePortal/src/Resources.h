af96b037c90e4bcc44a7f1a0471606a8255f1583
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-06-06 22:39:18 +0200

    CaptivePortal extracting to library - operation step by step 10

--- NodeMcuV3/src/libraries/CaptivePortal/src/Resources.h
+++ NodeMcuV3/src/libraries/CaptivePortal/src/Resources.h
@@ -4,5 +4,29 @@
 #endif //NODEMCUV3_CAPTIVEPORTAL_RESOURCES_H
 
 class Resources {
-
+public:
+  const String fetchStatusJs {
+    "var error = function(message, err) {"
+      "console.log(message, err);"
+      "var container = document.getElementById('error');"
+      "container.innerText = message + \". Caused by: \" + err;"
+      "container.style.display = null"
+    "};"
+    "var fetchStatus = function() {"
+      "return fetch('/status', {"
+        "method: 'get'"
+      "})"
+          ".then(function(response) {"
+            "if(!response.ok) {"
+              "throw new Error('Invalid status code: ' + response.status)"
+            "}"
+            "return response.json();"
+          "})"
+          ".catch(function(err) {"
+            "console.log(err);"
+            "error(\"Fetch template error!\", err);"
+            "throw err;"
+          "});"
+    "};"
+  };
 };
