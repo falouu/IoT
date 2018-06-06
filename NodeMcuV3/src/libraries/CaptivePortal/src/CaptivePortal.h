a23e86607722be7e76a6506691e2f08bfebbf77c
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-06-06 21:46:38 +0200

    CaptivePortal extracting to library - operation step by step 4

--- NodeMcuV3/src/libraries/CaptivePortal/src/CaptivePortal.h
+++ NodeMcuV3/src/libraries/CaptivePortal/src/CaptivePortal.h
@@ -14,6 +14,14 @@ private:
   void handleEnableAP();
   const String softAPSSID();
   bool isAPModeEnabled();
+
+  inline String getWifiStatus();
+  const String getWifiStatusText(unsigned int connStatus);
+  const String getLastWifiStatus();
+  inline bool isWifiConnected();
+  void connectWifi();
+  inline void updateLastWifiStatus(unsigned int connStatus);
+  inline void printWifiStatus(unsigned int connStatus);
 };
 
 #endif //NODEMCUV3_CAPTIVEPORTAL_H
