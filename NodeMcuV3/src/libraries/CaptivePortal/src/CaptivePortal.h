98faecd5401f6ba3d5466dc7b83286a8a3111e25
Marcin Falkowski <marcin.falkowski.pl@gmail.com>
2018-06-06 21:44:53 +0200

    CaptivePortal extracting to library - operation step by step 3

--- NodeMcuV3/src/libraries/CaptivePortal/src/CaptivePortal.h
+++ NodeMcuV3/src/libraries/CaptivePortal/src/CaptivePortal.h
@@ -1,10 +1,19 @@
 #ifndef NODEMCUV3_CAPTIVEPORTAL_H
 #define NODEMCUV3_CAPTIVEPORTAL_H
 
+#include <Arduino.h>
+
 class CaptivePortal {
 public:
   void setup();
   void handle();
+
+private:
+  void enableSoftAP();
+  void handleDisableAP();
+  void handleEnableAP();
+  const String softAPSSID();
+  bool isAPModeEnabled();
 };
 
 #endif //NODEMCUV3_CAPTIVEPORTAL_H
